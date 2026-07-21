443e1141cfa99743deb47d29ed22e77e6fd2ba55
Dan <alejoxdc@gmail.com>
2026-07-21 09:34:03 -0500

    rama 19

--- bascula/__init__.py
+++ bascula/__init__.py
@@ -7,3 +7,4 @@ from . import models
 def _post_init_assign_analytic_accounts(env):
     """Asigna cuentas analíticas a puntos operativos existentes."""
     env['secadora.lugar']._assign_analytic_accounts()
+
